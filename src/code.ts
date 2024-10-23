e9993af69247837facfcbce8787e46072ccea6a6
poi <poi@poinoMacBook-Pro.local>
2024-10-24 01:09:56 +0900

    delete: type file

--- src/code.ts
+++ src/code.ts
@@ -1,5 +1,33 @@
-// 型定義をインポート
-import {Book, GoogleBooksAPIResponse} from './types';
+/**
+ * 書籍情報の型定義
+ */
+interface Book {
+    book_id: number;
+    isbn: string;
+    storage_location: string;
+    label_type: string;
+    label_spec: string;
+    attachment_location_spec: string;
+    notes: string;
+}
+
+/**
+ * Google Books APIのレスポンス型定義
+ */
+interface GoogleBooksVolumeInfo {
+    title: string;
+    categories?: string[];
+}
+
+interface GoogleBooksItem {
+    volumeInfo: GoogleBooksVolumeInfo;
+}
+
+interface GoogleBooksAPIResponse {
+    totalItems: number;
+    items?: GoogleBooksItem[];
+}
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const fetchBooks = (): void => {
 
